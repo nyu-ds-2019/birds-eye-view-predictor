047804f4ff99821d71000ecb32b375f3e33e5fdf
Nikhil Supekar <nikhilvsupekar@gmail.com>
2020-05-12 22:41:19 -0400

    bug fix

--- integration_1/train_rm.sh
+++ integration_1/train_rm.sh
@@ -4,7 +4,7 @@ python3 train_rm_resnet.py
     --num-workers 2 \
     --learning-rate 1e-3 \
     --num-epochs 50 \
-    --train-views front_left,front,front_right,back_left,back_back_right
+    --train-views front_left,front,front_right,back_left,back_back_right \
     --ae-model ae_best_performing.pt \
     --data-directory ../data \
     --model-directory ../models
